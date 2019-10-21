305407835344cf4501065458b96c1555e5d5e746
VladislavNovak <54819099+VladislavNovak@users.noreply.github.com>
2019-10-22 00:51:43 +0300

    2110-3 js: открывание/закрывание формы на index
    + незначительные изменения в index: form в соответствии с pixel

---
+++ js/external.js
@@ -0,0 +1,12 @@
+// скрипт начинает работать в момент загрузки документа:
+window.onload = function() {
+	// получаем элемент, который будет управлять открытием формы:
+	var eLink = document.querySelector(".popup-opener");
+	// по клику запускаем функцию, которая...
+	eLink.addEventListener("click", function() {
+		// получет элемент формы и...
+		var ePopup = document.querySelector(".booking-form");	
+		// скрывает её, если у неё стиль display:flex или наоборот - прячет её:
+		ePopup.style.display = (ePopup.style.display == "none") ? "flex" : "none";
+	});
+}
