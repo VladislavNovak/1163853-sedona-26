840406485fb793597d1423122a506f95c9daef26
VladislavNovak <54819099+VladislavNovak@users.noreply.github.com>
2019-10-24 23:18:27 +0300

    2410-1 index: кнопка поиска; hotels: сортировка и гостиницы
    index:
    "поиск гостиниц" заменён с ссылки на button;
    в футере к ссылкам на соц.сети добавлен title;
    hotels:
    отредактирована последовательность заголовков h1-h2-h3;
    блок с сортировкой перенесён в блок с отелями;
    в блоке с гостиницами картинка в логической структуре располагается после описания самих гостиниц;
    исправлены ошибки переполнения в блоках сортировки и гостиниц

--- js/external.js
+++ js/external.js
@@ -3,7 +3,8 @@ window.onload = function() {
 	// получаем элемент, который будет управлять открытием формы:
 	var eLink = document.querySelector(".popup-opener");
 	// по клику запускаем функцию, которая...
-	eLink.addEventListener("click", function() {
+	eLink.addEventListener("click", function(evt) {
+		evt.preventDefault();
 		// получет элемент формы и...
 		var ePopup = document.querySelector(".booking-form");	
 		// скрывает её, если у неё стиль display:flex или наоборот - прячет её:
