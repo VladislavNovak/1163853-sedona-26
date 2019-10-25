57891540d02113e8bfd76ddc9ecb30e4da2ddd4c
VladislavNovak <54819099+VladislavNovak@users.noreply.github.com>
2019-10-25 19:00:32 +0300

    2510-2 сделана подложкак для карты
    + незначительные изменения в js по открытию формы в index

--- js/external.js
+++ js/external.js
@@ -1,13 +1,35 @@
 // скрипт начинает работать в момент загрузки документа:
 window.onload = function() {
+	//! index: открытие/закрытие формы
 	// получаем элемент, который будет управлять открытием формы:
 	var eLink = document.querySelector(".popup-opener");
+	var eCheckIn = document.querySelector("#check-in");
 	// по клику запускаем функцию, которая...
 	eLink.addEventListener("click", function(evt) {
 		evt.preventDefault();
 		// получет элемент формы и...
 		var ePopup = document.querySelector(".booking-form");	
 		// скрывает её, если у неё стиль display:flex или наоборот - прячет её:
-		ePopup.style.display = (ePopup.style.display == "none") ? "flex" : "none";
+		// ePopup.style.display = (ePopup.style.display == "none") ? "flex" : "none";
+		if(ePopup.style.display == "none") {
+			ePopup.style.display = "flex";
+			eCheckIn.focus();
+		} else {
+			ePopup.style.display = "none";
+		}
 	});
-}
+
+	//! index форма: проверка корректности введённой даты
+	var eIndexForm = document.querySelector("#hotel-selector");
+	var eCheckInValid = document.querySelector("#check-in");
+	var eCheckOutValid = document.querySelector("#check-out");
+	var eAdultValid = document.querySelector("#adult");
+	var eChildrenValid = document.querySelector("#children");
+
+	eIpdexForm.addEventListener("submit", function(evt){
+		if(!eCheckInValid||!eCheckInValid||!eAdultValid||!eChildrenValid) {
+			evt.preventDefault();
+			console.log("Нужно ввести корректные значения");
+		}
+	});
+};
