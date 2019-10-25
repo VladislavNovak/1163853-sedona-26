2ae123cab6df69b1889d6c0f7163de520cddaa15
VladislavNovak <54819099+VladislavNovak@users.noreply.github.com>
2019-10-26 02:05:58 +0300

    2510-3 js: анимация формы в index

--- js/external.js
+++ js/external.js
@@ -1,35 +1,68 @@
 // скрипт начинает работать в момент загрузки документа:
 window.onload = function() {
-	//! index: открытие/закрытие формы
+	// ! index: открытие/закрытие формы через клик на button !
 	// получаем элемент, который будет управлять открытием формы:
 	var eLink = document.querySelector(".popup-opener");
+	var eCheckInSetFocus = document.querySelector("#check-in");
 	var eCheckIn = document.querySelector("#check-in");
+	var eCheckOut = document.querySelector("#check-out");
+
 	// по клику запускаем функцию, которая...
 	eLink.addEventListener("click", function(evt) {
 		evt.preventDefault();
 		// получет элемент формы и...
 		var ePopup = document.querySelector(".booking-form");	
+
 		// скрывает её, если у неё стиль display:flex или наоборот - прячет её:
 		// ePopup.style.display = (ePopup.style.display == "none") ? "flex" : "none";
 		if(ePopup.style.display == "none") {
 			ePopup.style.display = "flex";
-			eCheckIn.focus();
+			eCheckInSetFocus.focus();
+
+			// возвращаем сохранённые данные:
+			eCheckIn.value = localStorage.getItem("checkInValue");
+			eCheckOut.value = localStorage.getItem("checkOutValue");
 		} else {
+			// сохраняем данные в localStorage
+			try {
+				localStorage.setItem("checkInValue", eCheckIn.value);
+				localStorage.setItem("checkOutValue", eCheckOut.value);
+			} catch(err) {}
+
+			// и закрываем форму
 			ePopup.style.display = "none";
+
+		}
+	});
+
+	// ! index: закрытие формы через ESC !
+	// получет элемент формы...
+	var ePopup = document.querySelector(".booking-form");
+	ePopup.addEventListener("keydown", function(evt) {
+		// если форма открыта и нажата ESC...
+		if(evt.keyCode === 27 && ePopup.style.display == "flex") { 
+			// <- if( ePopup.classList.contains("modal-show") ) {
+			// то форму можно свернуть
+			evt.preventDefault();
+			ePopup.style.display = "none";
+			// -> ePopup.classList.remove("modal-show");
 		}
 	});
 
 	//! index форма: проверка корректности введённой даты
-	var eIndexForm = document.querySelector("#hotel-selector");
-	var eCheckInValid = document.querySelector("#check-in");
-	var eCheckOutValid = document.querySelector("#check-out");
-	var eAdultValid = document.querySelector("#adult");
-	var eChildrenValid = document.querySelector("#children");
+	// var eIndexForm = document.querySelector("#hotel-selector");
+	// var eCheckInValid = document.querySelector("#check-in");
+	// var eCheckOutValid = document.querySelector("#check-out");
+	// var eAdultValid = document.querySelector("#adult");
+	// var eChildrenValid = document.querySelector("#children");
 
-	eIpdexForm.addEventListener("submit", function(evt){
-		if(!eCheckInValid||!eCheckInValid||!eAdultValid||!eChildrenValid) {
-			evt.preventDefault();
-			console.log("Нужно ввести корректные значения");
-		}
-	});
+	// eIndexForm.addEventListener("submit", function(evt){
+	// 	if(!eCheckInValid||!eCheckInValid||!eAdultValid||!eChildrenValid) {
+	// 		evt.preventDefault();
+	// 		console.log("Нужно ввести корректные значения");
+	// 	}
+	// });
+
+	// эксперименты с localStorage
+	// localStorage.setItem("name", "keks");
 };
