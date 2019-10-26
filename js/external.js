dc58689bba18165c06afc152193e5694be51b79c
VladislavNovak <54819099+VladislavNovak@users.noreply.github.com>
2019-10-27 01:53:44 +0300

    2610-2 js: выделил активные элементы в hotels
    externel.js - удалил код, который точно не будет использоваться;
    hotels -  добавил скрипт, который снимает выделение с некоторых элементов, которые были обозначены как "активные" при загрузке.

--- js/external.js
+++ js/external.js
@@ -2,67 +2,51 @@
 window.onload = function() {
 	// ! index: открытие/закрытие формы через клик на button !
 	// получаем элемент, который будет управлять открытием формы:
-	var eLink = document.querySelector(".popup-opener");
-	var eCheckInSetFocus = document.querySelector("#check-in");
-	var eCheckIn = document.querySelector("#check-in");
-	var eCheckOut = document.querySelector("#check-out");
+	var $ = getCls => { return document.querySelector(getCls); };
+
+	var popupOpener = 		$(".popup-opener");
+	var bookingForm = 		$(".booking-form");
+	var checkinOnload = 	$("#check-in");
+	var checkin = 			$("#check-in");
+	var checkout = 			$("#check-out");
+
+	
 
 	// по клику запускаем функцию, которая...
-	eLink.addEventListener("click", function(evt) {
+	popupOpener.addEventListener("click", function(evt) {
 		evt.preventDefault();
-		// получет элемент формы и...
-		var ePopup = document.querySelector(".booking-form");	
-
 		// скрывает её, если у неё стиль display:flex или наоборот - прячет её:
 		// ePopup.style.display = (ePopup.style.display == "none") ? "flex" : "none";
-		if(ePopup.style.display == "none") {
-			ePopup.style.display = "flex";
-			eCheckInSetFocus.focus();
+		if(bookingForm.style.display == "none") {
+			bookingForm.style.display = "flex";
+			checkinOnload.focus();
 
 			// возвращаем сохранённые данные:
-			eCheckIn.value = localStorage.getItem("checkInValue");
-			eCheckOut.value = localStorage.getItem("checkOutValue");
+			checkin.value = localStorage.getItem("checkInValue");
+			checkout.value = localStorage.getItem("checkOutValue");
 		} else {
 			// сохраняем данные в localStorage
 			try {
-				localStorage.setItem("checkInValue", eCheckIn.value);
-				localStorage.setItem("checkOutValue", eCheckOut.value);
+				localStorage.setItem("checkInValue", checkin.value);
+				localStorage.setItem("checkOutValue", checkout.value);
 			} catch(err) {}
 
 			// и закрываем форму
-			ePopup.style.display = "none";
+			bookingForm.style.display = "none";
 
 		}
 	});
 
 	// ! index: закрытие формы через ESC !
-	// получет элемент формы...
-	var ePopup = document.querySelector(".booking-form");
-	ePopup.addEventListener("keydown", function(evt) {
+	bookingForm.addEventListener("keydown", function(evt) {
 		// если форма открыта и нажата ESC...
-		if(evt.keyCode === 27 && ePopup.style.display == "flex") { 
+		if(evt.keyCode === 27 && bookingForm.style.display == "flex") { 
 			// <- if( ePopup.classList.contains("modal-show") ) {
 			// то форму можно свернуть
 			evt.preventDefault();
-			ePopup.style.display = "none";
+			bookingForm.style.display = "none";
 			// -> ePopup.classList.remove("modal-show");
 		}
 	});
 
-	//! index форма: проверка корректности введённой даты
-	// var eIndexForm = document.querySelector("#hotel-selector");
-	// var eCheckInValid = document.querySelector("#check-in");
-	// var eCheckOutValid = document.querySelector("#check-out");
-	// var eAdultValid = document.querySelector("#adult");
-	// var eChildrenValid = document.querySelector("#children");
-
-	// eIndexForm.addEventListener("submit", function(evt){
-	// 	if(!eCheckInValid||!eCheckInValid||!eAdultValid||!eChildrenValid) {
-	// 		evt.preventDefault();
-	// 		console.log("Нужно ввести корректные значения");
-	// 	}
-	// });
-
-	// эксперименты с localStorage
-	// localStorage.setItem("name", "keks");
 };
