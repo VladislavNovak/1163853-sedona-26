871fdaacb118f251c62bc717342c8f804a4ef51d
VladislavNovak <54819099+VladislavNovak@users.noreply.github.com>
2019-10-29 03:11:24 +0300

    2810-2 js: скрипты с разных страниц добавлены в external

--- js/external.js
+++ js/external.js
@@ -1,52 +1,93 @@
 // скрипт начинает работать в момент загрузки документа:
-window.onload = function() {
-	// ! index: открытие/закрытие формы через клик на button !
-	// получаем элемент, который будет управлять открытием формы:
-	var $ = getCls => { return document.querySelector(getCls); };
+window.addEventListener("load", 
+	function(evt) {
+		var popupOpener = document.querySelector(".popup-opener");
+		var bookingForm = document.querySelector(".booking-form");
+		var checkIn = document.querySelector("#check-in");
+		var checkOut = document.querySelector("#check-out");
 
-	var popupOpener = 		$(".popup-opener");
-	var bookingForm = 		$(".booking-form");
-	var checkinOnload = 	$("#check-in");
-	var checkin = 			$("#check-in");
-	var checkout = 			$("#check-out");
+		if (popupOpener) {
+			popupOpener.addEventListener ("click", function( evt ) {
+				if (bookingForm.style.display == "none") {
+					bookingForm.style.display = "flex"; 
+	     
+					if (!bookingForm.classList.contains("booking-form-appear")) {
+						bookingForm.classList.add("booking-form-appear");
+					}
 
-	
+					checkIn.focus();
 
-	// по клику запускаем функцию, которая...
-	popupOpener.addEventListener("click", function(evt) {
-		evt.preventDefault();
-		// скрывает её, если у неё стиль display:flex или наоборот - прячет её:
-		// ePopup.style.display = (ePopup.style.display == "none") ? "flex" : "none";
-		if(bookingForm.style.display == "none") {
-			bookingForm.style.display = "flex";
-			checkinOnload.focus();
+					// localStorage - возвращаем сохранённые данные:
+					checkIn.value = localStorage.getItem("checkInValue");
+					checkOut.value = localStorage.getItem("checkOutValue");
+				} else {     
+					try {
+						// localStorage - сохраняем данные
+						localStorage.setItem("checkInValue", checkIn.value);
+	              		localStorage.setItem("checkOutValue", checkOut.value);
+	            	} catch(err) {}
 
-			// возвращаем сохранённые данные:
-			checkin.value = localStorage.getItem("checkInValue");
-			checkout.value = localStorage.getItem("checkOutValue");
-		} else {
-			// сохраняем данные в localStorage
-			try {
-				localStorage.setItem("checkInValue", checkin.value);
-				localStorage.setItem("checkOutValue", checkout.value);
-			} catch(err) {}
+	            	if( bookingForm.classList.contains("booking-form-appear") ) {
+	              		bookingForm.classList.remove("booking-form-appear");
+	            	}
 
-			// и закрываем форму
-			bookingForm.style.display = "none";
+	            	// и закрываем форму
+	            	bookingForm.style.display = "none";
+	          		}
 
+				evt.preventDefault();
+	        	}
+			);
 		}
-	});
+		
+		//*********************************
 
-	// ! index: закрытие формы через ESC !
-	bookingForm.addEventListener("keydown", function(evt) {
-		// если форма открыта и нажата ESC...
-		if(evt.keyCode === 27 && bookingForm.style.display == "flex") { 
-			// <- if( ePopup.classList.contains("modal-show") ) {
-			// то форму можно свернуть
-			evt.preventDefault();
-			bookingForm.style.display = "none";
-			// -> ePopup.classList.remove("modal-show");
-		}
-	});
+		var relatedSorts = document.querySelectorAll(".sort-types");
+		var targetSort = document.querySelector(".sort-types.sort-active-onload");
+		var relatedLinks = document.querySelectorAll(".updown-link");
+		var targetLink = document.querySelector(".updown-link.updown-active-onload");
+		var events = ["click", "focus"];
 
-};
+		// relatedItems - группа связанных элементов
+		// events - список событий, которые вызывают последующие действия
+		// targetItem - элемент( из группы связанных ), который нужно деактивировать
+		// removeClass - класс, который нужно удалить из targetItem
+		function deactivator(relatedItems, events, targetItem, removeClass) {
+			// перечень элементов, выбираем один...
+			relatedItems.forEach( function(item) {   
+				// перечень событий, выбираем одно...            
+				events.forEach( function( event ) { 
+					// применяем к каждому элементу события по порядку...   
+					item.addEventListener(event, function( evt) {  
+						if (targetItem.classList.contains( removeClass )) {
+							// отключаем у заданного элемента класс со стилями     
+							targetItem.classList.remove( removeClass );
+						}
+						evt.preventDefault();
+	            	}); 
+	          	});
+	        });
+	    }
+
+      deactivator(relatedLinks, events, targetLink, "updown-active-onload");
+      deactivator(relatedSorts, events, targetSort, "sort-active-onload");
+      
+	//********************
+	}
+);
+
+
+	// // ! index: закрытие формы через ESC !
+	// bookingForm.addEventListener("keydown", 
+	// 	function(evt) {
+	// 		// если форма открыта и нажата ESC...
+	// 		if(evt.keyCode === 27 && bookingForm.style.display == "flex") { 
+	// 			// <- if( ePopup.classList.contains("modal-show") ) {
+	// 			// то форму можно свернуть
+	// 			evt.preventDefault();
+	// 			bookingForm.style.display = "none";
+	// 			// -> ePopup.classList.remove("modal-show");
+	// 		}
+	// 	}
+	// );
+
